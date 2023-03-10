141e4e0036fd44dc117545cef18bdefb6b4a69b4
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-03-10 17:41:47 -0500

    added short medical description

--- components/StoreComponents/ItemOverlay/ItemCustomizationPanel/SpecialItemComponents/MedicalItemDetails.tsx
+++ components/StoreComponents/ItemOverlay/ItemCustomizationPanel/SpecialItemComponents/MedicalItemDetails.tsx
@@ -2,11 +2,23 @@ import styled from 'styled-components';
 import { TMedicationInformation } from '../../../../../global';
 import BadgeBanner from '../../../BadgeBanner/BadgeBanner';
 
-const ItemCustomizationPanelItemDescriptionSpecial = styled.span`
+const MedicalItemDetailsSpanWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 7px;
+    margin-bottom: 11px;
+`;
+
+const MedicalItemDetailsSpan = styled.span`
     font-size: 14px;
     font-weight: 400;
     color: var(--quinary-gray);
     line-height: 1.4;
+`;
+
+const MedicalItemDetailsSpanGroup = styled.div`
+    display: flex;
+    column-gap: 18px;
 `;
 
 type TMedicalItemDetails = {
@@ -18,14 +30,28 @@ type TMedicalItemDetails = {
 export default function MedicalItemDetails({ primaryDescription, medicationInformation, specialDeliveryStatus }: TMedicalItemDetails) {
     return (
         <>
-            <ItemCustomizationPanelItemDescriptionSpecial>
-                {primaryDescription}
-            </ItemCustomizationPanelItemDescriptionSpecial>
-            <ItemCustomizationPanelItemDescriptionSpecial>
-                Last Filled:
-                {' '}
-                {medicationInformation?.lastFilledDate}
-            </ItemCustomizationPanelItemDescriptionSpecial>
+            <MedicalItemDetailsSpanWrapper>
+                <MedicalItemDetailsSpan>
+                    {primaryDescription}
+                </MedicalItemDetailsSpan>
+                <MedicalItemDetailsSpan>
+                    Last Filled:
+                    {' '}
+                    {medicationInformation?.lastFilledDate}
+                </MedicalItemDetailsSpan>
+                <MedicalItemDetailsSpanGroup>
+                    <MedicalItemDetailsSpan>
+                        Quantity:
+                        {' '}
+                        {medicationInformation?.quantity}
+                    </MedicalItemDetailsSpan>
+                    <MedicalItemDetailsSpan>
+                        Refills:
+                        {' '}
+                        {medicationInformation?.refills}
+                    </MedicalItemDetailsSpan>
+                </MedicalItemDetailsSpanGroup>
+            </MedicalItemDetailsSpanWrapper>
             <BadgeBanner specialDeliveryStatus={specialDeliveryStatus} />
 
         </>
