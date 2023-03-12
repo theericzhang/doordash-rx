cc90e422340aa3a55c83da38c495c2babe7874b0
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-03-12 19:45:28 -0400

    create FooterInsuranceInfo

---
+++ components/StoreComponents/ItemOverlay/ItemCustomizationPanel/SpecialItemComponents/Footer/FooterInsuranceInfo.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+type TFooterInsuranceInfo = {
+    insurer: string;
+    memberID: number;
+}
+
+export default function FooterInsuranceInfo() {
+    return (
+
+    );
+}
