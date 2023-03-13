fde99dd6a9e06f1a8e4ad8cc4fdd8a3b4cebe4ba
Eric Zhang <56938909+theericzhang@users.noreply.github.com>
2023-03-13 15:39:53 -0400

    conditionally render inputStepperButton for adder function based on isMedication

--- components/StoreComponents/CartItem/CartItem.tsx
+++ components/StoreComponents/CartItem/CartItem.tsx
@@ -102,6 +102,7 @@ export default function CartItem({
             </CartItemDescriptionWrapper>
             <InputStepper
                 itemID={itemID}
+                isMedication={isMedication}
             />
         </CartItemWrapper>
     );
